7f5a9163749407836338aa76a911728a61306f41
Ofri Kleinfeld <ofri64@gmail.com>
2019-11-19 13:06:00 +0200

    adds tokenization functionality

--- data_handle/comments_dataset.py
+++ data_handle/comments_dataset.py
@@ -1,11 +1,13 @@
-import torch.utils.data as data
 import pandas as pd
+import torch.utils.data as data
+from pytorch_pretrained_bert import BertTokenizer
 
 
 class CommentsDataset(data.Dataset):
     def __init__(self, filepath):
         super().__init__()
         self.filepath = filepath
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self.data = None
 
     def _load_file(self):
@@ -14,7 +16,13 @@ class CommentsDataset(data.Dataset):
 
     def __getitem__(self, item):
         self._load_file()
-        id_index, x, y = self.data.iloc[item]
+        id_index, comment_text, y = self.data.iloc[item]
+
+        # convert comment text to tokens and then to discrete token ids
+        text_bert_format = "[CLS] " + comment_text + " [SEP]"
+        tokenized_text = self.tokenizer.tokenize(text_bert_format)
+        x = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+
         return id_index, x, y
 
     def __len__(self):
